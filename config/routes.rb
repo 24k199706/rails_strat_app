07373992a8f4fb27596ecdb6ef18ecb455b327f2
24k199706 <24k0626ryo@gmail.com>
2021-07-25 16:15:57 +0900

    最初のコミット

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#home' ,as:"root"
   get "/help",to:"static_pages#help",as:"help"
   get "/contact",to:"static_pages#contact",as:"contact"
-  get "/about",to:"static_pages#about",as:"about"
-  get "/signup",to:"user#new",as:"signup"
+  get "/about",to:"static_pages#about",as:"about"  
   get "/user/:id",to:"user#show",as:"show"
+  get "/signup",to:"user#new",as:"signup"
+  post  "/signup",to:"user#creat"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_050105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
