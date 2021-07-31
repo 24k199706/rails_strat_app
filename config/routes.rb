618f2b3fa62da9862f0defa1e775c900bf8b509b
24k199706 <24k0626ryo@gmail.com>
2021-07-31 16:23:07 +0900

    任意のメッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   get "/help",to:"static_pages#help",as:"help"
   get "/contact",to:"static_pages#contact",as:"contact"
   get "/about",to:"static_pages#about",as:"about"  
-  get "/user/:id",to:"user#show",as:"show"
-  get "/signup",to:"user#new",as:"signup"
-  post  "/signup",to:"user#creat"
+  get "/user/:id",to:"users#show",as:"show"
+  get "/signup",to:"users#new",as:"signup"
+  post  "/signup",to:"users#creat"
+  get '/login',to:"sessions#new",as:"login"
+  post '/login',to: "sessions#creat"
+  delete '/login',to:"sessions#destroy"
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
