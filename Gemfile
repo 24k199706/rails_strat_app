b7dce3ea1e78a2f265bb722fcee628737a928356
24k199706 <24k0626ryo@gmail.com>
2021-08-08 11:58:25 +0900

    fight

--- Gemfile
+++ Gemfile
@@ -57,3 +57,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,22 @@ Rails.application.routes.draw do
   root 'static_pages#home' ,as:"root"
   get "/help",to:"static_pages#help",as:"help"
   get "/contact",to:"static_pages#contact",as:"contact"
-  get "/about",to:"static_pages#about",as:"about"  
+  get "/about",to:"static_pages#about",as:"about"
+  get "/user",to:"users#index",as:"index"
   get "/user/:id",to:"users#show",as:"show"
   get "/signup",to:"users#new",as:"signup"
   post  "/signup",to:"users#creat"
+  patch "/user/:id",to:"users#update",as:"update"
+  delete "/user/:id",to:"users#destroy",as:"delete"
+  get "/user/:id/edit",to:"users#edit",as:"edit"
   get '/login',to:"sessions#new",as:"login"
   post '/login',to: "sessions#creat"
   delete '/logout',to:"sessions#destroy", as:"logout"
+  post "/microposts",to:"microposts#create",as:"microposts"
+  delete "/microposts/:id",to:"microposts#destroy",as:"micropost"
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,only: [:new, :create, :edit, :update]
+  resources :microposts,only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
